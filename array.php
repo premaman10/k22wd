c42416fa5097ff56f877ac6dbbe808c831ef82e5
amanp <amanprem374@gmail.com>
2024-09-03 09:13:13 +0530

    Add files via upload

--- array.php
+++ array.php
@@ -5,7 +5,7 @@
 $cars = array("Volvo","BMW","Toyota");
 // var_dump ($cars);
 // echo"<pre>";
-print_r($cars);
+print_r($cars);// it print array's element with there indexes
 // echo"</pre>";
 // for($x=0;$x<=2;$x++){
 //     echo $cars[$x]."<br>";
